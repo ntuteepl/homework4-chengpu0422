9a3c4cfc254000f5b322b80e8115db459414a713
chengpu0422 <146065192+chengpu0422@users.noreply.github.com>
2023-10-30 19:18:21 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,i,c;
+    scanf("%d",&n);
+    if(n>=1&&n<=50)
+    {
+        int i,c;
+        int a[50];
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+
+            for(c=2;c<a[i];c++)
+            {
+                if(a[i]%c==0)
+                {
+                    printf("NO ");
+                    break;
+                }
+            }
+                if(c==a[i])
+                {
+                    printf("YES ");
+                }
+            }
+
+    }
+    else
+    {
+        printf("fail");
+    }
+
+}
 
